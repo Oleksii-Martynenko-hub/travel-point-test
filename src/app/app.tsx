506247f271176cdd8b415811774983fb3d56f85e
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2024-01-18 14:59:05 +0200

    Added routes to app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
 import styles from './app.module.scss';
 
 export function App() {
   return (
     <div
-    ></div>
+      className={styles.container}
+    >
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/user-profile/:userId" element={<div>User</div>} />
+      </Routes>
+    </div>
   );
 }
 
