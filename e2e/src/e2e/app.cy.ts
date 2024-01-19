2d96c2dcecdc6537a0aa713813a845c36b98bbaa
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2024-01-19 13:55:47 +0200

    Added e2e test to home page

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,70 @@
-import { getGreeting } from '../support/app.po';
+import {
+  getDetailsLink,
+  getTitle,
+  getUserEmail,
+  getUserItem,
+  getUserList,
+  getUserName,
+  getUserWebsite,
+} from '../support/app.po';
 
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  let firstUser: { name: string; email: string; website: string } | null = null;
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  it('should display title message', () => {
+    getTitle().contains(/Список користувачів сайту/);
+  });
+
+  it('should request users', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/users',
+    }).as('usersDataCheck');
+
+    cy.wait('@usersDataCheck').then((interception) => {
+      const { response } = interception;
+      if (!response) {
+        throw new Error('API call has no response');
+      }
+      assert.isArray(response.body, 'API call has users data');
+      assert.equal(response.body.length, 10, 'API call has users data');
+      assert.isObject(response.body[0], 'API call has users data');
+      assert.hasAllKeys(
+        response.body[0],
+        [
+          'id',
+          'name',
+          'username',
+          'email',
+          'website',
+          'phone',
+          'address',
+          'company',
+        ],
+        'API call has users data'
+      );
+
+      firstUser = response.body[0];
+    });
+  });
+
+  it('should contain user item with correct data and details link', () => {
+    getUserList().within(() => {
+      getUserItem()
+        .first()
+        .within(() => {
+          if (firstUser) {
+            getUserName().contains(firstUser?.name);
+            getUserEmail().contains(firstUser?.email);
+            getUserWebsite().contains(firstUser?.website);
+          }
+
+          getDetailsLink()
+            .contains('Деталі')
+            .should('have.attr', 'href', '/user-profile/1');
+        });
+    });
   });
 });
--- e2e/src/support/app.po.ts
+++ e2e/src/support/app.po.ts
@@ -1,1 +1,9 @@
-export const getGreeting = () => cy.get('h1');
+export const getTitle = () => cy.get('h1');
+
+export const getUserList = () => cy.get('ul[class*="user-list"]');
+export const getUserItem = () => cy.get('li[class*="user-item"]');
+export const getUserName = () => cy.get('p[class*="user-name"]');
+export const getUserEmail = () => cy.get('a[class*="email-link"]');
+export const getUserWebsite = () => cy.get('a[class*="website-link"]');
+
+export const getDetailsLink = () => cy.get('a[class*="details-link"]');
