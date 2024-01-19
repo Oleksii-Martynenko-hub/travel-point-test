9a25f55f90f99d192c4313792eb88ab9076adfff
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2024-01-19 11:39:19 +0200

    Added user-profile details with styles

--- src/app/pages/user-profile/user-profile.tsx
+++ src/app/pages/user-profile/user-profile.tsx
@@ -1,6 +1,7 @@
-import { Link, redirect, useParams } from 'react-router-dom';
+import { redirect, useParams } from 'react-router-dom';
 
-import { UserType } from 'src/app/components/common/hooks/useFetchUserList';
+import { UserType } from '../../components/common/hooks/useFetchUserList';
+import StyledLink from '../../components/common/styled-link/styled-link';
 
 import styles from './user-profile.module.scss';
 
@@ -24,13 +25,49 @@ export function UserProfile({ userList }: UserProfileProps) {
 
   if (!user) return <h3>User doesn't exist!</h3>;
 
+  const { name, email, website, address, phone, company, username } = user;
+
   return (
-    <div className={styles['container']}>
-      <Link to="/">Back</Link>
-      <h1>
-        Welcome to {user.username}'s Profile!
-        <p>{JSON.stringify(user)}</p>
-      </h1>
+    <div className={styles['user-profile']}>
+      <div className={styles['back-link-wrapper']}>
+        <StyledLink to="/">Back</StyledLink>
+      </div>
+
+      <h2 className={`title ${styles['user-name']}`}>{name}</h2>
+      <div className={styles['user-details-wrapper']}>
+        <p className={styles.property}>website:</p>
+        <a
+          className={styles['website-link']}
+          href={`https://${website}`}
+          target="blank"
+        >
+          {website}
+        </a>
+
+        <p className={styles.property}>username:</p>
+        <p>{username}</p>
+
+        <p className={styles.property}>email:</p>
+        <a className={styles['email-link']} href={`mailto:${email}`}>
+          {email}
+        </a>
+
+        <p className={styles.property}>company:</p>
+        <p>{company.name}</p>
+
+        <p className={styles.property}>phone:</p>
+        <a
+          className={styles['website-link']}
+          href={`phone:${phone.split(' ')[0]}`}
+        >
+          {phone.split(' ')[0]}
+        </a>
+
+        <p className={styles.property}>address:</p>
+        <p>
+          {address.suite}, {address.street}, {address.city}
+        </p>
+      </div>
     </div>
   );
 }
