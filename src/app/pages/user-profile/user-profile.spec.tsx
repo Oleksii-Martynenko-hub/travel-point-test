d71e65517d77367939c6948fcf41f787b5578bba
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2024-01-22 14:51:13 +0200

    Fixed user-profile component tests

--- src/app/pages/user-profile/user-profile.spec.tsx
+++ src/app/pages/user-profile/user-profile.spec.tsx
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { renderWithProviders } from '../../../utils/store-provider-test';
 
 import UserProfile from './user-profile';
 
@@ -31,48 +32,65 @@ const mockUser2 = JSON.parse(JSON.stringify(mockUser));
 mockUser2.id = 2;
 mockUser2.name = 'Test Name 2';
 
-const mockComponent = (userList = [mockUser], userId = 1) => (
-  <MemoryRouter initialEntries={[`/user-profile/${userId}`]}>
-    <Routes>
-      <Route
-        path="/user-profile/:userId"
-        element={<UserProfile userList={userList} />}
-      />
-    </Routes>
-  </MemoryRouter>
-);
+const initialUsers = {
+  ids: [mockUser.id, mockUser2.id],
+  entities: { [mockUser.id]: mockUser, [mockUser2.id]: mockUser2 },
+  loadingStatus: 'loaded' as const,
+};
+
+const MockComponent = ({ userId = 1 }) => {
+  return (
+    <MemoryRouter initialEntries={[`/user-profile/${userId}`]}>
+      <Routes>
+        <Route path="/user-profile/:userId" element={<UserProfile />} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
 
 describe('UserProfile', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(mockComponent());
+    const { baseElement } = renderWithProviders(<MockComponent />);
     expect(baseElement).toBeTruthy();
   });
 
   it('should render correct user data', async () => {
-    const { unmount, getByRole } = render(mockComponent());
+    const { unmount, getByRole } = renderWithProviders(<MockComponent />, {
+      preloadedState: {
+        users: initialUsers,
+      },
+    });
 
     expect(getByRole('heading', { name: mockUser.name })).toBeTruthy();
     unmount();
 
-    render(mockComponent([mockUser, mockUser2], 2));
+    renderWithProviders(<MockComponent userId={2} />, {
+      preloadedState: {
+        users: initialUsers,
+      },
+    });
 
     expect(getByRole('heading', { name: mockUser2.name })).toBeTruthy();
   });
 
   it('should render back link', async () => {
-    const { getByRole } = render(mockComponent());
+    const { getByRole } = renderWithProviders(<MockComponent />);
 
     expect(getByRole('link', { name: 'Back' })).toBeTruthy();
   });
 
   it('should render user doesn`t exist', async () => {
-    const { unmount, getByRole } = render(mockComponent([]));
+    const { unmount, getByRole } = renderWithProviders(<MockComponent />);
 
     expect(getByRole('heading', { name: "User doesn't exist!" })).toBeTruthy();
     expect(getByRole('link', { name: 'Back' })).toBeTruthy();
     unmount();
 
-    render(mockComponent([mockUser, mockUser2], 99));
+    renderWithProviders(<MockComponent userId={99} />, {
+      preloadedState: {
+        users: initialUsers,
+      },
+    });
 
     expect(getByRole('heading', { name: "User doesn't exist!" })).toBeTruthy();
     expect(getByRole('link', { name: 'Back' })).toBeTruthy();
